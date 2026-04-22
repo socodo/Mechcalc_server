5f9229a8348f429419ba036f25e41deb653c7d22
hoainguyenegn <153376071+hoainguyenegn@users.noreply.github.com>
2026-04-22 19:41:08 +0700

    feat(auth): add request DTOs for Google login and token refresh

---
+++ src/main/java/com/socodo/mechcalc/dto/request/RefreshRequest.java
@@ -0,0 +1,21 @@
+package com.socodo.mechcalc.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RefreshRequest {
+
+    @NotBlank(message = "Refresh token is required")
+    String token;
+    
+}
